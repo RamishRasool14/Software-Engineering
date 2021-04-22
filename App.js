d28f7ca0300b18fdf9c2d079fed57feb5dfd6c45
Hamad Jamal <66657772+hamadJamal@users.noreply.github.com>
2021-04-23 04:08:46 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import firebase from "firebase";
+var postRef = firebase.database().ref('posts')
 
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
@@ -8,23 +9,6 @@ import AppNavigator from './src/navigations/Navigator.js';
 
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
-
-var firebaseConfig = {
-  apiKey: "AIzaSyD3vMo4Ain8ErPTljr1MXyoMalVXLu7OAw",
-  authDomain: "se-project-81a89.firebaseapp.com",
-  databaseURL: "https://se-project-81a89-default-rtdb.firebaseio.com",
-  projectId: "se-project-81a89",
-  storageBucket: "se-project-81a89.appspot.com",
-  messagingSenderId: "17700065874",
-  appId: "1:17700065874:web:e3191757efc54234b9458b"
-};
-firebase.initializeApp(firebaseConfig)
-// Initialize Firebase
-// if (firebase.apps.length === 0) {
-//   firebase.initializeApp(firebaseConfig);
-// }
-// console.log(firebase)
-
 
 
 
